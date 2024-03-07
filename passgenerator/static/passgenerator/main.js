e3a01d8e3211466f69ccd620509425b01e81f149
myke <81974207+heismyke@users.noreply.github.com>
2024-03-07 14:09:10 +0100

    secureme

--- passgenerator/static/passgenerator/main.js
+++ passgenerator/static/passgenerator/main.js
@@ -1,6 +1,9 @@
+
 const generated_text = document.getElementById('generated_text_input')
 const copy_button = document.getElementById('copy')
 const refresh_btn = document.getElementById('refresh')
+
+const progress_bar = document.getElementById('progress_bar')
 
 copy_button.addEventListener('click', () => {
     generated_text.select()
@@ -21,3 +24,16 @@ copy_button.addEventListener('click', () => {
 refresh_btn.addEventListener('click', () => {
     window.location.reload()
 })
+
+const password_length = generated_text.value.length
+const max_length = 50
+const progress_percentage = (password_length / max_length) * 100
+progress_bar.style.setProperty('--progress-width', `${progress_percentage}%`);
+
+if(password_length < 8){
+    progress_bar.style.setProperty('--progress-color', 'orange');
+}else if(password_length >=8 && password_length < 20){
+    progress_bar.style.setProperty('--progress-color', 'lightgreen');
+}else if(password_length >=20 ){
+    progress_bar.style.setProperty('--progress-color', 'green');
+}
