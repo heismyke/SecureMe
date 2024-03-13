733773dbca352442b359a9cbd251d5ce58a2a8fd
myke <81974207+heismyke@users.noreply.github.com>
2024-03-13 02:29:25 +0100

    recent changes

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,16 @@
 source venv/bin/activate
+
+
+set -e
+
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+python manage.py collectstatic
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+# Other commands in your script
